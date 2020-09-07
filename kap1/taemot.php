065f1c5ee9d088fbf3337411a597bbe21a92e79f
hugo <hugoericson@hotmail.com>
2020-09-07 09:33:32 +0200

    practice

---
+++ kap1/taemot.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    echo $_REQUEST["anamn"];
+    echo "<br>";
+    echo $_REQUEST["epost"];
+    echo "<br>";
+    echo $_REQUEST["tele"];
+    echo "<br>";
+    echo $_REQUEST["ja"];
+    echo "<br>";
+    echo $_REQUEST["kontakt"];
+    ?>
+</body>
+</html>
